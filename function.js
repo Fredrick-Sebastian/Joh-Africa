affce00dee4fc3ae61048cb2e38e345b402af463
Fredrick Sebastian <futurefamily154@gmail.com>
2026-03-10 17:04:54 +0300

    Add files via upload

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
+const sidebar = document.querySelector('.sidebar')
+
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
@@ -10,4 +12,47 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slides').prepend(items[items.length - 1])
 })
+
+var swiper = new Swiper(".swiper",{
+    effect:"coverflow",
+    grabCursor:true,
+    centeredSlides:true,
+    coverflowEffect:{
+        rotate:0,
+        stretch:0,
+        depth:100,
+        modifier:4,
+        slideShadows:true
+    },
+    loop:true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    keyboard:{
+        enabled:true
+    },
+    mousewheel:{
+        thresholDelta:70
+    },
+    breakpoints:{
+        560:{
+            slidesPerView: 2.5
+        },
+        768:{
+            slidesPerView: 3
+        },
+        1024:{
+            slidePerView: 3
+        }
+    }
+});
+
+function showSidebar(){
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    sidebar.style.display = 'none'
+}
 
